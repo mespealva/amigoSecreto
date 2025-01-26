e80a1598ccaea5c3c8e858e2c20c3fba94cfcc9e
Maria A <maria.esperanza.alga@gmail.com>
2025-01-26 20:13:55 -0300

    cambios

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 /* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 Aquí deberás desarrollar la lógica para resolver el problema.*/
-//llenar la lista al momento de agregar un nombre   check
-//poner el ganador en la pag   check
-//poner el alert cuando esta vacio check
-//quizas verificar q sea un nombre no repetido
+
 
 
 let amigo;
@@ -13,27 +10,39 @@ var indice
 var ganador;
 
 function asignarTextoElementos(elemento, texto) {
-    
     let elementoHTML = document.getElementById(elemento)
     elementoHTML.innerHTML += texto;
     return;
 }
 
+function botones(boton, nombre) {
+    let elemento = document.getElementById(boton)
+    
+    //document.getElementById('botonSorteo').setAttribute('disabled', true);
+    //let palabra = document.getElementById(nombre)
+    elemento.innerText = nombre
+    elemento.setAttribute('disable', true)
+}
+
 function agregarAmigo() {
-    //console.log(nombres.length)
+    
     let amigo = document.getElementById('amigo').value
+
     if( amigo == ''){
+        //no permitir valores vacios
         alert('debe ingresar un nombre');
     }else{
         if(nombres.includes(amigo)){
+            //alertar valores repetidos
             alert(`${amigo} ya se encuentra en tu lista`)
             limpiarCaja()
         }else{
-           nombres.push(amigo)
-        NumAmigos = nombres.length
-        console.log(nombres)
-        asignarTextoElementos("listaAmigos",`<li> ${amigo} </li>`);
-        limpiarCaja() 
+            //agregar nombres a la lista
+            nombres.push(amigo)
+            NumAmigos = nombres.length
+            //mostrar nombres
+            asignarTextoElementos("listaAmigos",`<li> ${amigo} </li>`);
+            limpiarCaja() 
         }
         
     }
@@ -46,25 +55,36 @@ function limpiarCaja() {
 }
 
 function sortearAmigo() {
-    document.getElementById('agregar').setAttribute('disabled', true);
-
+    //deshabilitar el boton de agregar una vez q inicia el sorteo
+    //document.getElementById('agregar').setAttribute('disabled', true);
+    //botones('agregar', 'Ya sorteaste' )
     limpiarCaja()
+
     if (nombres.length === 0 ){
+        //alertar si no hay nombres en la lista
         alert('no haz ingresado nombres')
     }else{
+        //elegir ganador
         let indice = Math.floor(Math.random()* NumAmigos)
         ganador = nombres[indice]
-        console.log(ganador)
-        lista = document.getElementById('listaAmigos')
-        lista.remove()
-        asignarTextoElementos("resultado",`el ganador es ${ganador}!!`);
-        document.getElementById('botonSorteo').setAttribute('disabled', true);
-
         
-        //nombres.slice()
+        //limpiar la lista en la pantalla
+        lista = document.querySelector('li')
+        lista.remove()
+        
+        //mostrar ganador
+        asignarTextoElementos("resultado",`el ganador es ${ganador}!!`);
+        //una vez realizado el sorteo deshabilitar el boton
+        
+        botones('botonSorteo', 'listo el sorteo')
+        //document.getElementById('botonSorteo').setAttribute('disabled', true);
+        //document.getElementById('botonSorteo').innerText = "Ya realisaste el sorteo"
+        
+        //console.log(ganador)
         //console.log(nombres)
     }
     
     return ganador
 }
+//pense agregar un boton para empezar denuevo, pero no lo pedian
 
