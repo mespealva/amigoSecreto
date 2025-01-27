76db7b698ad84b9f8e59be9a29cad7a26897b0ea
Maria A <maria.esperanza.alga@gmail.com>
2025-01-27 12:25:49 -0300

    app1

---
+++ app1.js
@@ -0,0 +1,31 @@
+
+
+/*function sortearAmigo() {
+
+    
+    let btn= document.getElementById("botonSorteo");
+    btn.setAttribute("data-click",true);
+    btn.addEventListener("click",function(){
+    if(eval( btn.getAttribute("data-click"))){
+        console.log('1')
+        alert('hh')
+       btn.setAttribute("data-click",false);
+    }else{
+        
+        location.reload()
+      //console.log('2')
+      //alert('bbbb')
+
+    }
+    
+    })
+       
+      
+}
+sortearAmigo()
+    */
+
+function sortearAmigo() {
+    alert('bbb')
+    
+}
